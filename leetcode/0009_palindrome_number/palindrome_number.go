606af288cd33009bbbbb1cb903b711e79866c2b4
ming.yang <ming.yang@12301.cn>
2022-07-22 09:41:06 +0800

    commit

--- leetcode/0009_palindrome_number/palindrome_number.go
+++ leetcode/0009_palindrome_number/palindrome_number.go
@@ -1,10 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(isPalindrome(1000))
 }
 
 func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	if x < 9 {
+		return true
+	}
 
-	return false
+	if x%10 == 0 {
+		return false
+	}
+
+	rightPart := 0
+	for x > rightPart {
+		rightPart = rightPart*10 + x%10
+		x = x / 10
+	}
+
+	return x == rightPart || x == rightPart/10
 }
