150535f7c55cff5fbead8e1571b9f0b39908243c
ming.yang <ming.yang@12301.cn>
2022-09-08 13:55:46 +0800

    add

---
+++ leetcode/0050_powx-n/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def myPow(self, x:float, n: int) -> float:
+        return 0.0
+
+
+solution = Solution()
+print(solution.myPow(2.00000, 10))
+print(solution.myPow(2.100000, 3))
+print(solution.myPow(2.00000, -2))
