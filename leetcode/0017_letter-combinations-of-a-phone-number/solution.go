eb3aba6020649c0a5626408c4b62b470214c8fcf
ming.yang <ming.yang@12301.cn>
2023-04-12 14:26:42 +0800

    leet 17

--- leetcode/0017_letter-combinations-of-a-phone-number/solution.go
+++ leetcode/0017_letter-combinations-of-a-phone-number/solution.go
@@ -1,9 +1,41 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(letterCombinations("239"))
+}
+
+var combinations []string
+var letterMapping = map[string]string{
+	"2": "abc",
+	"3": "def",
+	"4": "ghi",
+	"5": "jkl",
+	"6": "mno",
+	"7": "pqrs",
+	"8": "tuv",
+	"9": "wxyz",
 }
 
 func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
+	combinations = []string{}
+	backtrack(digits, 0, "")
+	return combinations
+}
 
+func backtrack(digits string, index int, combination string) {
+	if index == len(digits) {
+		combinations = append(combinations, combination)
+	} else {
+		digit := string(digits[index])
+		letters := letterMapping[digit]
+		lettersLen := len(letters)
+		for i := 0; i < lettersLen; i++ {
+			backtrack(digits, index+1, combination+string(letters[i]))
+		}
+	}
 }
