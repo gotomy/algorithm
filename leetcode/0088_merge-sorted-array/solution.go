0221aa661bd520314cb8a476a2ee74b4251bc3da
ming.yang <ming.yang@12301.cn>
2023-04-19 14:46:31 +0800

    leetcode 0088

---
+++ leetcode/0088_merge-sorted-array/solution.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	copy(nums1[m:], nums2)
+	sort.Ints(nums1)
+}
